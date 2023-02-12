9fe8cf9ad0ac7328ebfef61d8103284605a1955b
Mohammad Salim <mhmadip@gmail.com>
2023-02-12 11:51:24 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,57 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(XylophoneApp());
-
+void main(){
+  runApp(XylophoneApp());
+}
 class XylophoneApp extends StatelessWidget {
-  void playSound(int soundNumber) {
-   final player = AudioPlayer();
-   player.play(UrlSource('note$soundNumber.wav'));
-
-  // If file located in assets folder like assets/sounds/note01.wave"
-  await player.play(AssetSource('sounds/note$soundNumber.wav'));
-  }
-// its better to not use function as given below, but using a StatelessWidget
-  Expanded buildKey(int soundNo, MaterialStateColor materialStateColor) {
-    return Expanded(
-      child: TextButton(
-        style: ButtonStyle(backgroundColor: materialStateColor),
-        onPressed: () {
-          playSound(soundNo);
-        },
-      ),
-    );
-  }
+  const XylophoneApp
+({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
+      home:Scaffold(
+        backgroundColor:Colors.black,
+        body:SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              buildKey(
-                  1, MaterialStateColor.resolveWith((states) => Colors.red)),
-              buildKey(
-                  2, MaterialStateColor.resolveWith((states) => Colors.yellow)),
-              buildKey(
-                  3, MaterialStateColor.resolveWith((states) => Colors.green)),
-              buildKey(4,
-                  MaterialStateColor.resolveWith((states) => Colors.blueGrey)),
-              buildKey(
-                  5, MaterialStateColor.resolveWith((states) => Colors.blue)),
-              buildKey(
-                  6, MaterialStateColor.resolveWith((states) => Colors.purple)),
-              buildKey(
-                  7,
-                  MaterialStateColor.resolveWith(
-                      (states) => Colors.pinkAccent)),
-            ],
-          ),
-        ),
-      ),
+            crossAxisAlignment:CrossAxisAlignment.stretch,
+            children:[
+              BuildKey(color:Colors.red,fileName:1),
+              BuildKey(color:Colors.black,fileName:1),
+              BuildKey(color:Colors.purple,fileName:1),
+              BuildKey(color:Colors.green,fileName:1),
+              BuildKey(color:Colors.orange,fileName:1),
+
+            ]
+          )
+        )
+      )
+    );
+  }
+}
+
+class BuildKey extends StatelessWidget {
+  final Color? color;
+  final int? fileName;
+   BuildKey({
+    super.key,
+    this.color,
+    this.fileName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child:     ElevatedButton(
+      onPressed:(){
+        
+          final player = AudioPlayer();
+            player.play(AssetSource('a$fileName.wav'));
+        },
+      child:Text(''),
+      style:ButtonStyle(
+        backgroundColor:MaterialStateProperty.all(color),
+      )
+    )
     );
   }
 }
